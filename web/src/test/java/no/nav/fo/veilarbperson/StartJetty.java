386d0e3081df5d49126a42a15534bf88209ef6d9
June Henriksen <june.henriksen2@nav.no>
2016-12-19 15:04:18 +0100

    [PK-35494] Sett opp jetty
    PKULV-2008

---
+++ web/src/test/java/no/nav/fo/veilarbperson/StartJetty.java
@@ -0,0 +1,20 @@
+package no.nav.fo.veilarbperson;
+
+import no.nav.sbl.dialogarena.common.jetty.Jetty;
+
+import static no.nav.sbl.dialogarena.common.jetty.Jetty.usingWar;
+import static no.nav.sbl.dialogarena.common.jetty.JettyStarterUtils.*;
+
+public class StartJetty {
+
+    private static final int PORT = 8488;
+
+    public static void main(String []args) {
+        Jetty jetty = usingWar()
+                .at("/veilarbperson")
+                .port(PORT)
+                .buildJetty();
+        jetty.startAnd(first(waitFor(gotKeypress())).then(jetty.stop));
+    }
+
+}
