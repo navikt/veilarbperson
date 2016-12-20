bc5c8a23ac5e1db5a3ca3369a94338f9b691c82c
June Henriksen <june.henriksen2@nav.no>
2016-12-20 09:30:14 +0100

    [PK-35494] Oppdater versjon av mor-pom
    PKULV-2008

--- web/src/main/java/no/nav/fo/veilarbperson/IsAliveServlet.java
+++ web/src/main/java/no/nav/fo/veilarbperson/IsAliveServlet.java
@@ -1,11 +1,17 @@
 package no.nav.fo.veilarbperson;
 
+import org.slf4j.Logger;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+import static org.slf4j.LoggerFactory.getLogger;
+
 public class IsAliveServlet extends HttpServlet {
+
+    private static final Logger logger = getLogger(IsAliveServlet.class);
 
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
