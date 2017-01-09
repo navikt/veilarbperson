2c7cd5008d39690f79088934ada5a4e7126fc942
June Henriksen <june.henriksen2@nav.no>
2017-01-09 12:11:34 +0100

    [PK-35479] Fjern ubrukt kode som gir sonarfeil

--- web/src/main/java/no/nav/fo/veilarbperson/APIController.java
+++ web/src/main/java/no/nav/fo/veilarbperson/APIController.java
@@ -1,9 +1,14 @@
 package no.nav.fo.veilarbperson;
 
+import org.slf4j.Logger;
 import org.springframework.web.bind.annotation.*;
+
+import static org.slf4j.LoggerFactory.getLogger;
 
 @RestController
 public class APIController {
+
+    private static final Logger logger = getLogger(APIController.class);
 
     @RequestMapping(value = "/person/{fnr}", produces = "application/json")
     public String person(@PathVariable String fnr) {
@@ -12,8 +17,11 @@ public class APIController {
 
     private String getPerson(String fnr) {
 
+        logger.info("Henter person med fnr:" + fnr );
+
         return "{\"fornavn\": \"Liv\"," +
-                "\"etternavn\": \"Ullmann\"}";
+                "\"etternavn\": \"Ullmann\"," +
+                "\"fnr\" : \""+ fnr + "\"}";
     }
 }
 
