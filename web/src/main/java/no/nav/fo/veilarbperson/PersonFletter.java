0af4b5ad5b878296aabef7afdd9e36e9aec3b85e
Joakim Lindquister <joakim.lindquister@nav.no>
2017-01-18 16:01:20 +0100

    PK-35479, PKULV-2005 Hent sikkerhetsbegrensninger på person fra TPS

--- web/src/main/java/no/nav/fo/veilarbperson/PersonFletter.java
+++ web/src/main/java/no/nav/fo/veilarbperson/PersonFletter.java
@@ -1,7 +1,9 @@
 package no.nav.fo.veilarbperson;
 
+import no.nav.fo.veilarbperson.domain.Sikkerhetstiltak;
 import no.nav.fo.veilarbperson.services.PersonData;
 import no.nav.fo.veilarbperson.services.PersonService;
+import no.nav.tjeneste.virksomhet.person.v2.HentSikkerhetstiltakPersonIkkeFunnet;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class PersonFletter {
@@ -12,6 +14,12 @@ public class PersonFletter {
     public PersonData hentPerson(String fnr){
         PersonData personData = personService.hentPerson(fnr);
 
+        try {
+            Sikkerhetstiltak sikkerhetstiltak = personService.hentSikkerhetstiltak(fnr);
+            personData.medSikkerhetstiltak(sikkerhetstiltak.getSikkerhetstiltaksbeskrivelse());
+        } catch (HentSikkerhetstiltakPersonIkkeFunnet hentSikkerhetstiltakPersonIkkeFunnet) {
+            hentSikkerhetstiltakPersonIkkeFunnet.printStackTrace();
+        }
         //TODO: Fyll personData med mer data fra TPS, Digital kontaktinfo. norg2, felles kodeverk og TPSWS-egensatt
 
         return personData;
