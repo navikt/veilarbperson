e6ff44c8a99ef12d560acbf7fdc009b8a150313b
Aksel Wester <aksel.wester@nav.no>
2017-01-23 12:34:37 +0100

    Legger til kjonn og fødselsdato på Familiemedlem, hentet fra personnummer

--- web/src/main/java/no/nav/fo/veilarbperson/services/PersonDataMapper.java
+++ web/src/main/java/no/nav/fo/veilarbperson/services/PersonDataMapper.java
@@ -1,11 +1,12 @@
 package no.nav.fo.veilarbperson.services;
 
 import no.nav.fo.veilarbperson.domain.Sivilstand;
-import no.nav.tjeneste.virksomhet.organisasjonenhet.v1.informasjon.WSEnhet;
 import no.nav.tjeneste.virksomhet.person.v2.informasjon.*;
 
 import java.text.SimpleDateFormat;
-import java.util.*;
+import java.util.GregorianCalendar;
+import java.util.List;
+import java.util.Optional;
 
 import static java.util.Optional.ofNullable;
 import static java.util.stream.Collectors.toList;
@@ -92,17 +93,66 @@ class PersonDataMapper{
         return null;
     }
 
+    private static String personnummerTilFodselsdato(String personnummer) {
+        final String aar = personnummerTilAarstall(personnummer);
+        final String maaned = personnummerTilMaaned(personnummer);
+        final String dag = personnummerTilDag(personnummer);
+        return aar + "-" + maaned + "-" + dag;
+    }
+
+    private static String personnummerTilAarstall(String personnummer) {
+        int aarsiffer = Integer.parseInt(personnummer.substring(4, 6));
+        int individsiffer = Integer.parseInt(personnummer.substring(6, 9));
+        int aarstall;
+        if (individsiffer < 500) {
+            aarstall = 1900 + aarsiffer;
+        } else if (individsiffer < 750 && aarsiffer >= 54) {
+            aarstall = 1800 + aarsiffer;
+        } else if (individsiffer < 900 || aarsiffer < 40) {
+            aarstall = 2000 + aarsiffer;
+        } else {
+            aarstall = 1900 + aarsiffer;
+        }
+        return Integer.toString(aarstall);
+    }
+
+    private static String personnummerTilMaaned(String personnummer) {
+        int maanedsiffer = Integer.parseInt(personnummer.substring(2, 4));
+        if (maanedsiffer > 12) {
+            maanedsiffer -= 40;
+        }
+        return Integer.toString(maanedsiffer);
+    }
+
+    private static String personnummerTilDag(String personnummer) {
+        int dagsiffer = Integer.parseInt(personnummer.substring(0, 2));
+        if (dagsiffer > 31) {
+            dagsiffer -= 40;
+        }
+        return Integer.toString(dagsiffer);
+    }
+
     private static Familiemedlem familierelasjonTilFamiliemedlem(WSFamilierelasjon familierelasjon) {
 
         WSPerson person = familierelasjon.getTilPerson();
+        final String personnummer = person.getIdent().getIdent();
 
         return new Familiemedlem()
                 .withFornavn(person.getPersonnavn().getFornavn())
                 .withEtternavn(person.getPersonnavn().getEtternavn())
                 .withSammensattnavn(person.getPersonnavn().getSammensattNavn())
                 .withHarSammeBosted(familierelasjon.isHarSammeBosted())
-                .withPersonnummer(person.getIdent().getIdent());
+                .withPersonnummer(personnummer)
+                .withFodselsdato(personnummerTilFodselsdato(personnummer))
+                .withKjoenn(personnummerTilKjoenn(personnummer));
+    }
 
+    private static String personnummerTilKjoenn(String personnummer) {
+        if (Integer.parseInt(personnummer.substring(8, 9)) % 2 == 0) {
+            return "K";
+        } else {
+            return "M";
+        }
     }
 
     private static Sivilstand hentSivilstand(WSPerson person) {
