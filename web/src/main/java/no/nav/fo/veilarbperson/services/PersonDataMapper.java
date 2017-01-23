7eda16b5d92e454a92d006d2332197403e884760
Aksel Wester <aksel.wester@nav.no>
2017-01-23 13:15:52 +0100

    Trekker personnummerfunksjoner ut i egen util-klasse

--- web/src/main/java/no/nav/fo/veilarbperson/services/PersonDataMapper.java
+++ web/src/main/java/no/nav/fo/veilarbperson/services/PersonDataMapper.java
@@ -10,7 +10,8 @@ import java.util.Optional;
 
 import static java.util.Optional.ofNullable;
 import static java.util.stream.Collectors.toList;
-
+import static no.nav.fo.veilarbperson.utils.Personnummer.personnummerTilFodselsdato;
+import static no.nav.fo.veilarbperson.utils.Personnummer.personnummerTilKjoenn;
 
 class PersonDataMapper{
 
@@ -93,45 +94,6 @@ class PersonDataMapper{
         return null;
     }
 
-    private static String personnummerTilFodselsdato(String personnummer) {
-        final String aar = personnummerTilAarstall(personnummer);
-        final String maaned = personnummerTilMaaned(personnummer);
-        final String dag = personnummerTilDag(personnummer);
-        return aar + "-" + maaned + "-" + dag;
-    }
-
-    private static String personnummerTilAarstall(String personnummer) {
-        int aarsiffer = Integer.parseInt(personnummer.substring(4, 6));
-        int individsiffer = Integer.parseInt(personnummer.substring(6, 9));
-        int aarstall;
-        if (individsiffer < 500) {
-            aarstall = 1900 + aarsiffer;
-        } else if (individsiffer < 750 && aarsiffer >= 54) {
-            aarstall = 1800 + aarsiffer;
-        } else if (individsiffer < 900 || aarsiffer < 40) {
-            aarstall = 2000 + aarsiffer;
-        } else {
-            aarstall = 1900 + aarsiffer;
-        }
-        return Integer.toString(aarstall);
-    }
-
-    private static String personnummerTilMaaned(String personnummer) {
-        int maanedsiffer = Integer.parseInt(personnummer.substring(2, 4));
-        if (maanedsiffer > 12) {
-            maanedsiffer -= 40;
-        }
-        return Integer.toString(maanedsiffer);
-    }
-
-    private static String personnummerTilDag(String personnummer) {
-        int dagsiffer = Integer.parseInt(personnummer.substring(0, 2));
-        if (dagsiffer > 31) {
-            dagsiffer -= 40;
-        }
-        return Integer.toString(dagsiffer);
-    }
-
     private static Familiemedlem familierelasjonTilFamiliemedlem(WSFamilierelasjon familierelasjon) {
 
         WSPerson person = familierelasjon.getTilPerson();
@@ -145,14 +107,6 @@ class PersonDataMapper{
                 .withPersonnummer(personnummer)
                 .withFodselsdato(personnummerTilFodselsdato(personnummer))
                 .withKjoenn(personnummerTilKjoenn(personnummer));
-    }
-
-    private static String personnummerTilKjoenn(String personnummer) {
-        if (Integer.parseInt(personnummer.substring(8, 9)) % 2 == 0) {
-            return "K";
-        } else {
-            return "M";
-        }
     }
 
     private static Sivilstand hentSivilstand(WSPerson person) {
