794ea50cb5a1f94455d749022c24032b19b4e008
Aksel Wester <aksel.wester@nav.no>
2017-01-23 14:30:15 +0100

    Gjør flere tall om til konstanter

--- web/src/main/java/no/nav/fo/veilarbperson/utils/Personnummer.java
+++ web/src/main/java/no/nav/fo/veilarbperson/utils/Personnummer.java
@@ -5,6 +5,9 @@ public class Personnummer {
     public static final int INDIVIDNUMMER_1900_TIL_1999 = 500;
     public static final int INDIVIDNUMMER_1854_TIL_1899 = 750;
     public static final int INDIVIDSIFFER_1940_TIL_1999 = 900;
+    public static final int SISTE_MAANED = 12;
+    public static final int SISTE_DAG_I_MAANED = 31;
+    public static final int D_OG_H_NUMMER_OFFSET = 40;
 
     public static String personnummerTilKjoenn(String personnummer) {
         if (Integer.parseInt(personnummer.substring(8, 9)) % 2 == 0) {
@@ -44,16 +47,16 @@ public class Personnummer {
 
     private static String personnummerTilMaaned(String personnummer) {
         int maanedsiffer = Integer.parseInt(personnummer.substring(2, 4));
-        if (maanedsiffer > 12) {
-            maanedsiffer -= 40;
+        if (maanedsiffer > SISTE_MAANED) {
+            maanedsiffer -= D_OG_H_NUMMER_OFFSET;
         }
         return String.format("%02d", maanedsiffer);
     }
 
     private static String personnummerTilDag(String personnummer) {
         int dagsiffer = Integer.parseInt(personnummer.substring(0, 2));
-        if (dagsiffer > 31) {
-            dagsiffer -= 40;
+        if (dagsiffer > SISTE_DAG_I_MAANED) {
+            dagsiffer -= D_OG_H_NUMMER_OFFSET;
         }
         return String.format("%02d", dagsiffer);
     }
