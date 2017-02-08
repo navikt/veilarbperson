5604f5318fdb591b1bf3e54b34b4adc356ead007
June Henriksen <june.henriksen2@nav.no>
2017-02-08 10:40:44 +0100

    [kaizen] Formatter kode og rydd i imports

--- web/src/main/java/no/nav/fo/veilarbperson/APIController.java
+++ web/src/main/java/no/nav/fo/veilarbperson/APIController.java
@@ -1,11 +1,11 @@
 package no.nav.fo.veilarbperson;
 
 import no.nav.fo.veilarbperson.consumer.digitalkontaktinformasjon.DigitalKontaktinformasjonService;
-import no.nav.fo.veilarbperson.domain.PersonData;
 import no.nav.fo.veilarbperson.consumer.kodeverk.KodeverkService;
 import no.nav.fo.veilarbperson.consumer.organisasjonenhet.EnhetService;
 import no.nav.fo.veilarbperson.consumer.tps.EgenAnsattService;
 import no.nav.fo.veilarbperson.consumer.tps.PersonService;
+import no.nav.fo.veilarbperson.domain.PersonData;
 import org.slf4j.Logger;
 import org.springframework.web.bind.annotation.*;
 
