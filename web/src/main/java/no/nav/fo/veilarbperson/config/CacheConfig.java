61aa6d522a2499874e791b62cfdc7fec19bc126f
June Henriksen <june.henriksen2@nav.no>
2017-02-16 14:53:20 +0100

    [kaizen] Sett opp spring config for cache med import
    Vær konsekvent på hvordan springkonfigureringen gjøres

--- web/src/main/java/no/nav/fo/veilarbperson/config/CacheConfig.java
+++ web/src/main/java/no/nav/fo/veilarbperson/config/CacheConfig.java
@@ -5,12 +5,10 @@ import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.jcache.JCacheCacheManager;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 import javax.cache.spi.CachingProvider;
 import java.net.URISyntaxException;
 
-@Configuration
 @EnableCaching
 public class CacheConfig {
     @Bean
