8391c1dc09ea3cc122a05f1387c5a19b820bf39a
June Henriksen <june.henriksen2@nav.no>
2017-02-16 12:51:58 +0100

    [F17HL2-14] Sett opp caching for kodeverk [WIP]
    Dette virker ikke

---
+++ web/src/main/java/no/nav/fo/veilarbperson/config/CacheConfig.java
@@ -0,0 +1,27 @@
+package no.nav.fo.veilarbperson.config;
+
+
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.jcache.JCacheCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.cache.spi.CachingProvider;
+import java.net.URISyntaxException;
+
+@Configuration
+@EnableCaching
+public class CacheConfig {
+    @Bean
+    public CacheManager cacheManager() throws URISyntaxException {
+        CachingProvider cachingProvider = javax.cache.Caching.getCachingProvider();
+        javax.cache.CacheManager cacheManager = cachingProvider.getCacheManager(
+                getClass().getResource("/cache/ehcache.xml").toURI(),
+                cachingProvider.getDefaultClassLoader());
+
+        return new JCacheCacheManager(cacheManager);
+    }
+
+}
+
