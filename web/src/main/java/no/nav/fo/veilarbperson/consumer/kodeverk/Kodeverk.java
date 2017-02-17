8c602e605f648f463a99ef1cb100e94e9f8b1f0e
Skjefstad, Vegard <vegard.skjefstad@nav.no>
2017-02-17 11:52:32 +0100

    F17HL2-14: Supresser warning for denne fordi den må være public for at ehcache skal fungere.

--- web/src/main/java/no/nav/fo/veilarbperson/consumer/kodeverk/Kodeverk.java
+++ web/src/main/java/no/nav/fo/veilarbperson/consumer/kodeverk/Kodeverk.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Stream;
 
+@SuppressWarnings("WeakerAccess")
 public class Kodeverk {
 
     private final List<XMLKode> koder;
