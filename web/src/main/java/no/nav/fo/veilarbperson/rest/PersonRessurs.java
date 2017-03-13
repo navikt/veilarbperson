70cc626b253a62fa077ae5335bf4912beace64be
Hein Haraldsen <hein.haraldsen@nav.no>
2017-03-13 12:49:33 +0100

    PK-38701, PKULV-2141, PKULV-2146 Lagt på integrasjon mot ABAC

--- web/src/main/java/no/nav/fo/veilarbperson/rest/PersonRessurs.java
+++ web/src/main/java/no/nav/fo/veilarbperson/rest/PersonRessurs.java
@@ -1,5 +1,6 @@
 package no.nav.fo.veilarbperson.rest;
 
+import no.nav.brukerdialog.security.context.SubjectHandler;
 import no.nav.fo.veilarbperson.PersonFletter;
 import no.nav.fo.veilarbperson.consumer.digitalkontaktinformasjon.DigitalKontaktinformasjonService;
 import no.nav.fo.veilarbperson.consumer.kodeverk.KodeverkService;
@@ -8,6 +9,9 @@ import no.nav.fo.veilarbperson.consumer.tps.EgenAnsattService;
 import no.nav.fo.veilarbperson.consumer.tps.PersonService;
 import no.nav.fo.veilarbperson.domain.Feilmelding;
 import no.nav.fo.veilarbperson.domain.person.PersonData;
+import no.nav.sbl.dialogarena.common.abac.pep.Pep;
+import no.nav.sbl.dialogarena.common.abac.pep.domain.response.BiasedDecisionResponse;
+import no.nav.sbl.dialogarena.common.abac.pep.domain.response.Decision;
 import no.nav.tjeneste.virksomhet.person.v2.HentKjerneinformasjonPersonIkkeFunnet;
 import no.nav.tjeneste.virksomhet.person.v2.HentKjerneinformasjonSikkerhetsbegrensning;
 import org.slf4j.Logger;
@@ -20,18 +24,22 @@ import javax.ws.rs.core.Response.Status;
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 import static org.slf4j.LoggerFactory.getLogger;
 @Component
-@Path("/person/{personnummer}")
+@Path("/person/{fnr}")
 public class PersonRessurs {
 
     private static final Logger logger = getLogger(PersonRessurs.class);
 
     final PersonFletter personFletter;
+    final private Pep pep;
 
     public PersonRessurs(EnhetService enhetService,
                          DigitalKontaktinformasjonService digitalKontaktinformasjonService,
                          PersonService personService,
                          EgenAnsattService egenAnsattService,
-                         KodeverkService kodeverkService) {
+                         KodeverkService kodeverkService,
+                         Pep pep) {
+
+        this.pep = pep;
 
         personFletter = new PersonFletter(
                 enhetService,
@@ -43,22 +51,27 @@ public class PersonRessurs {
 
     @GET
     @Produces(APPLICATION_JSON)
-    public Response person(@PathParam("personnummer") String personnummer) {
+    public Response person(@PathParam("personnummer") String fnr) {
 
-        logger.info("Henter persondata med personnummer: " + personnummer);
+        logger.info("Henter persondata med fnr: " + fnr);
 
+        final String ident = SubjectHandler.getSubjectHandler().getUid();
+        BiasedDecisionResponse callAllowed = pep.isServiceCallAllowedWithIdent(ident, "veilarb", fnr);
+        if (callAllowed.getBiasedDecision().equals(Decision.Deny)) {
+            throw new NotAuthorizedException(ident + " doesn't have access to " + fnr);
+        }
         try {
-            PersonData person = personFletter.hentPerson(personnummer);
+            PersonData person = personFletter.hentPerson(fnr);
             return Response.ok().entity(person).build();
         } catch (HentKjerneinformasjonPersonIkkeFunnet hentKjerneinformasjonPersonIkkeFunnet) {
-            Feilmelding feilmelding = new Feilmelding("Fant ikke person med personnummer: " + personnummer,
+            Feilmelding feilmelding = new Feilmelding("Fant ikke person med fnr: " + fnr,
                     hentKjerneinformasjonPersonIkkeFunnet.toString());
             return Response
                     .status(Status.NOT_FOUND)
                     .entity(feilmelding)
                     .build();
         } catch (HentKjerneinformasjonSikkerhetsbegrensning hentKjerneinformasjonSikkerhetsbegrensning) {
-            Feilmelding feilmelding = new Feilmelding("Saksbehandler har ikke tilgang til personnummer: " + personnummer,
+            Feilmelding feilmelding = new Feilmelding("Saksbehandler har ikke tilgang til fnr: " + fnr,
                     hentKjerneinformasjonSikkerhetsbegrensning.toString());
             return Response
                     .status(Status.UNAUTHORIZED)
