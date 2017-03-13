66bb99b6d0c3281eed4c9f99f96dc2feaf47c8fd
Hein Haraldsen <hein.haraldsen@nav.no>
2017-03-13 12:49:33 +0100

    PK-38701, PKULV-2141, PKULV-2146 Lagt på integrasjon mot ABAC

--- web/src/main/java/no/nav/fo/veilarbperson/config/ApplicationConfig.java
+++ web/src/main/java/no/nav/fo/veilarbperson/config/ApplicationConfig.java
@@ -2,13 +2,15 @@ package no.nav.fo.veilarbperson.config;
 
 import no.nav.fo.veilarbperson.PersonFletter;
 import org.springframework.context.annotation.*;
+import no.nav.sbl.dialogarena.common.abac.pep.context.AbacContext;
 
 @Configuration
 @ComponentScan(basePackages = "no.nav.fo.veilarbperson")
 @Import({
         ServiceConfig.class,
         PersonFletter.class,
-        CacheConfig.class
+        CacheConfig.class,
+        AbacContext.class
 })
 public class ApplicationConfig {
 
