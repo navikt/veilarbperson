22378f6c5758b9175a0c2de20ab2ef91698db2fc
Jan Berge Ommedal <jan.berge.ommedal@nav.no>
2017-07-21 14:31:02 +0200

    Full overgang til api-app

--- web/src/test/java/no/nav/fo/veilarbperson/StartJetty.java
+++ web/src/test/java/no/nav/fo/veilarbperson/StartJetty.java
@@ -1,38 +1,21 @@
 package no.nav.fo.veilarbperson;
 
-import no.nav.brukerdialog.security.context.JettySubjectHandler;
+import no.nav.dialogarena.config.DevelopmentSecurity;
 import no.nav.sbl.dialogarena.common.jetty.Jetty;
-import no.nav.sbl.dialogarena.test.SystemProperties;
-import org.apache.geronimo.components.jaspi.AuthConfigFactoryImpl;
 
-import javax.security.auth.message.config.AuthConfigFactory;
-
-import static java.security.Security.setProperty;
 import static no.nav.sbl.dialogarena.common.jetty.Jetty.usingWar;
 
 class StartJetty {
 
-    private static final int PORT = 8488;
+    private static final int PORT = 8438;
 
     public static void main(String []args) {
-
-        setupAutentisering();
-
-        Jetty jetty = usingWar()
-                .at("/veilarbperson")
-                .port(PORT)
-                .loadProperties("/environment-test.properties")
-                .overrideWebXml()
-                .configureForJaspic()
-                .buildJetty();
-        jetty.start();
+        DevelopmentSecurity.setupISSO(usingWar()
+                        .at("/veilarbperson")
+                        .port(PORT)
+                        .loadProperties("/environment-test.properties")
+                , new DevelopmentSecurity.ISSOSecurityConfig("veilarbperson", "t6")
+        ).buildJetty().start();
     }
 
-    private static void setupAutentisering() {
-        SystemProperties.setFrom("environment-test.properties");
-        System.setProperty("develop-local", "true");
-        System.setProperty("no.nav.modig.core.context.subjectHandlerImplementationClass", JettySubjectHandler.class.getName());
-        System.setProperty("org.apache.geronimo.jaspic.configurationFile", "web/src/test/resources/jaspiconf.xml");
-        setProperty(AuthConfigFactory.DEFAULT_FACTORY_SECURITY_PROPERTY, AuthConfigFactoryImpl.class.getCanonicalName());
-    }
 }
