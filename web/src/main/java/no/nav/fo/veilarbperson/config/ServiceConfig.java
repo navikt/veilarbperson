52aa62a722a2671677fc459955df538e0632972f
Joakim Lindquister <joakim.lindquister@nav.no>
2017-08-18 15:11:09 +0200

    OPP-32 Bruk ny versjon av Organisasjonsenhet

--- web/src/main/java/no/nav/fo/veilarbperson/config/ServiceConfig.java
+++ web/src/main/java/no/nav/fo/veilarbperson/config/ServiceConfig.java
@@ -10,21 +10,21 @@ import no.nav.sbl.dialogarena.common.abac.pep.Pep;
 import no.nav.tjeneste.pip.egen.ansatt.v1.EgenAnsattV1;
 import no.nav.tjeneste.virksomhet.digitalkontaktinformasjon.v1.DigitalKontaktinformasjonV1;
 import no.nav.tjeneste.virksomhet.kodeverk.v2.KodeverkPortType;
-import no.nav.tjeneste.virksomhet.organisasjonenhet.v1.OrganisasjonEnhetV1;
+import no.nav.tjeneste.virksomhet.organisasjonenhet.v2.OrganisasjonEnhetV2;
 import no.nav.tjeneste.virksomhet.person.v3.PersonV3;
 import org.springframework.context.annotation.Bean;
 
 public class ServiceConfig {
 
     private final PersonV3 personV3;
-    private final OrganisasjonEnhetV1 organisasjonenhet;
+    private final OrganisasjonEnhetV2 organisasjonenhet;
     private final DigitalKontaktinformasjonV1 digitalKontaktinformasjonV1;
     private final EgenAnsattV1 egenAnsattV1;
     private final KodeverkPortType kodeverkPortType;
     private final Pep pep;
 
     public ServiceConfig(PersonV3 personV3,
-                         OrganisasjonEnhetV1 organisasjonenhet,
+                         OrganisasjonEnhetV2 organisasjonenhet,
                          DigitalKontaktinformasjonV1 digitalKontaktinformasjonV1,
                          EgenAnsattV1 egenAnsattV1,
                          KodeverkPortType kodeverkPortType,
@@ -47,7 +47,6 @@ public class ServiceConfig {
         return new EgenAnsattService(egenAnsattV1);
     }
 
-
     @Bean
     EnhetService enhetService() {
         return new EnhetService(organisasjonenhet);
@@ -67,4 +66,5 @@ public class ServiceConfig {
     PepClient pepClient() {
         return new PepClient(pep);
     }
+
 }
