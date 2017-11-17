c5b8101af2e8a9d13a698f580d0b1f20b0effa3e
Christian Finstad <christian.finstad@nav.no>
2017-11-17 13:26:11 +0100

    Må spesifisere wsdl for kodeverk

--- web/src/main/java/no/nav/fo/veilarbperson/consumer/kodeverk/KodeverkConfig.java
+++ web/src/main/java/no/nav/fo/veilarbperson/consumer/kodeverk/KodeverkConfig.java
@@ -46,6 +46,7 @@ public class KodeverkConfig {
 
     private CXFClient<KodeverkPortType> factory() {
         return new CXFClient<>(KodeverkPortType.class)
+                .wsdl("classpath:kodeverk/no/nav/tjeneste/virksomhet/kodeverk/v2/Kodeverk.wsdl")
                 .address(getProperty("kodeverk.endpoint.url"))
                 .withProperty(MUST_UNDERSTAND, false);
     }
