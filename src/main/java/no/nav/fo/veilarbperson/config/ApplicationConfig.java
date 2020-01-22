624dc5afe2bcb6bc588eb30c88f2cf6992509523
Alexander Gård <alex.gaard@hotmail.com>
2020-01-22 10:11:12 +0100

    FO-2687 La til validering av Azure AD tokens

--- src/main/java/no/nav/fo/veilarbperson/config/ApplicationConfig.java
+++ src/main/java/no/nav/fo/veilarbperson/config/ApplicationConfig.java
@@ -2,6 +2,8 @@ package no.nav.fo.veilarbperson.config;
 
 import no.nav.apiapp.ApiApplication;
 import no.nav.apiapp.config.ApiAppConfigurator;
+import no.nav.brukerdialog.security.domain.IdentType;
+import no.nav.brukerdialog.security.oidc.provider.AzureADB2CConfig;
 import no.nav.common.auth.SecurityLevel;
 import no.nav.dialogarena.aktor.AktorConfig;
 import no.nav.fo.veilarbperson.PersonFletter;
@@ -10,6 +12,9 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import static no.nav.brukerdialog.security.Constants.AZUREADB2C_OIDC_COOKIE_NAME_FSS;
+import static no.nav.sbl.util.EnvironmentUtils.getRequiredProperty;
 
 @Configuration
 @EnableScheduling
@@ -26,8 +31,19 @@ public class ApplicationConfig implements ApiApplication {
 
     @Override
     public void configure(ApiAppConfigurator apiAppConfigurator) {
+        String discoveryUrl = getRequiredProperty("AAD_DISCOVERY_URL");
+        String clientId = getRequiredProperty("VEILARBLOGIN_AAD_CLIENT_ID");
+
+        AzureADB2CConfig config = AzureADB2CConfig.builder()
+                .discoveryUrl(discoveryUrl)
+                .expectedAudience(clientId)
+                .identType(IdentType.InternBruker)
+                .tokenName(AZUREADB2C_OIDC_COOKIE_NAME_FSS)
+                .build();
+
         apiAppConfigurator
                 .validateAzureAdExternalUserTokens(SecurityLevel.Level4)
+                .validateAzureAdInternalUsersTokens(config)
                 .issoLogin() // OpenAM i FSS
                 .sts();
     }
