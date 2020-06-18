d85a486ff5e87a6628766c290afbe887d656b0f7
Alexander Gård <alex.gaard@hotmail.com>
2020-06-18 15:10:30 +0200

    FO-2932 Legg på /ping slik at veilarbproxy for gjort helsesjekk

--- src/test/java/no/nav/veilarbperson/config/ControllerTestConfig.java
+++ src/test/java/no/nav/veilarbperson/config/ControllerTestConfig.java
@@ -2,12 +2,14 @@ package no.nav.veilarbperson.config;
 
 import no.nav.veilarbperson.controller.InternalController;
 import no.nav.veilarbperson.controller.PersonController;
+import no.nav.veilarbperson.controller.PingController;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
 @Import({
         PersonController.class,
-        InternalController.class
+        InternalController.class,
+        PingController.class
 })
 public class ControllerTestConfig {}
