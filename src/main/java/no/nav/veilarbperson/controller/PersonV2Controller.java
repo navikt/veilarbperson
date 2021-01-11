0f9b2d1b8532bd51a6c8ec97b176fe11dcabf412
Sneha Desai <sneha.desai@nav.no>
2021-01-11 10:26:56 +0100

    FO-3023: La pathvariable i hentPerson operasjonen

--- src/main/java/no/nav/veilarbperson/controller/PersonV2Controller.java
+++ src/main/java/no/nav/veilarbperson/controller/PersonV2Controller.java
@@ -6,10 +6,7 @@ import no.nav.veilarbperson.client.pdl.PersonV2Data;
 import no.nav.veilarbperson.service.AuthService;
 import no.nav.veilarbperson.service.PersonV2Service;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v2/person")
@@ -26,7 +23,7 @@ public class PersonV2Controller {
 
     @GetMapping("/{fodselsnummer}")
     @ApiOperation(value = "Henter informasjon om en person fra PDL")
-    public PersonV2Data hentPerson(@RequestParam("fnr") String fnr) throws Exception {
+    public PersonV2Data hentPerson(@PathVariable("fodselsnummer") String fnr) throws Exception {
         authService.stoppHvisEksternBruker();
         authService.sjekkLesetilgang(Fnr.of(fnr));
         return personV2Service.hentFlettetPerson(fnr, authService.getInnloggetBrukerToken());
