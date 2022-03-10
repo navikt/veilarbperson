42344f7f3e22a89dc86789d64eb261695a1b4bb4
Michael Gundersen <21148282+theagilepadawan@users.noreply.github.com>
2022-03-10 10:51:50 +0100

    Hent 'NAVident'-claim og bruk dette på spørring til veilarbpep, om det er satt

--- src/main/java/no/nav/veilarbperson/service/AuthService.java
+++ src/main/java/no/nav/veilarbperson/service/AuthService.java
@@ -6,10 +6,13 @@ import no.nav.common.auth.context.AuthContextHolder;
 import no.nav.common.client.aktorregister.AktorregisterClient;
 import no.nav.common.types.identer.AktorId;
 import no.nav.common.types.identer.Fnr;
+import no.nav.common.types.identer.NavIdent;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Optional;
 
 @Service
 public class AuthService {
@@ -53,6 +56,13 @@ public class AuthService {
 
     public boolean harLesetilgang(Fnr fnr) {
         AktorId aktorId = aktorregisterClient.hentAktorId(fnr);
+
+        Optional<NavIdent> navIdent = authContextHolder.getNavIdent();
+
+        if (navIdent.isPresent()) {
+            return veilarbPep.harVeilederTilgangTilPerson(navIdent.get(), ActionId.READ, aktorId);
+        }
+
         return veilarbPep.harTilgangTilPerson(getInnloggetBrukerToken(), ActionId.READ, aktorId);
     }
 
