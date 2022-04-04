ec5341977ed1a94940564060163d186359de4cf4
Mathias S. Müller <mathias.sundt.muller@nav.no>
2022-04-04 12:14:30 +0200

    Bump noen avhengigheter

--- src/test/java/no/nav/veilarbperson/config/ApplicationTestConfig.java
+++ src/test/java/no/nav/veilarbperson/config/ApplicationTestConfig.java
@@ -11,7 +11,6 @@ import org.springframework.context.annotation.Import;
 
 @Configuration
 @Import({
-        SwaggerConfig.class,
         ClientTestConfig.class,
         ControllerTestConfig.class,
         ServiceTestConfig.class,
