afcf2746dd72fd1cd84c87e1e7c997277d81160f
Mathias S. Müller <mathias.sundt.muller@nav.no>
2022-05-13 14:25:16 +0200

    Endre testconfigfiler

--- src/test/java/no/nav/veilarbperson/config/PdlClientTestConfig.java
+++ src/test/java/no/nav/veilarbperson/config/PdlClientTestConfig.java
@@ -1,9 +1,14 @@
 package no.nav.veilarbperson.config;
 
+import com.github.tomakehurst.wiremock.client.WireMock;
 import com.github.tomakehurst.wiremock.junit.WireMockRule;
+import com.github.tomakehurst.wiremock.matching.AnythingPattern;
+import com.github.tomakehurst.wiremock.matching.StringValuePattern;
 import no.nav.veilarbperson.client.pdl.PdlClientImpl;
 import no.nav.veilarbperson.utils.TestUtils;
 import org.junit.Rule;
+
+import java.util.Arrays;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
@@ -13,22 +18,25 @@ public class PdlClientTestConfig {
     @Rule
     public WireMockRule wireMockRule = new WireMockRule(0);
 
-    public String configurApiResponse(String responseFilename) {
-        String hentPersonResponseJson = TestUtils.readTestResourceFile(responseFilename);
-        String apiUrl = "http://localhost:" + wireMockRule.port();
+    public String getPdlApiUrl() {
+        return "http://localhost:" + wireMockRule.port();
+    }
 
-        givenThat(post(anyUrl())
+    public PdlClientImpl getPdlClient() {
+        return new PdlClientImpl(getPdlApiUrl());
+    }
+
+    public void configurePdlResponse(String responseFile, String... personer) {
+        String hentPersonResponseJson = TestUtils.readTestResourceFile(responseFile);
+
+        StringValuePattern pattern = Arrays.stream(personer).map(WireMock::containing).reduce(WireMock::or).orElse(
+                new AnythingPattern());
+
+        givenThat(post(WireMock.urlEqualTo("/graphql"))
+                .withRequestBody(pattern)
                 .willReturn(aResponse()
                         .withStatus(200)
                         .withBody(hentPersonResponseJson))
         );
-
-        return apiUrl;
     }
-
-    public PdlClientImpl configurPdlClient(String responseFile) {
-        String apiUrl = configurApiResponse(responseFile);
-        return new PdlClientImpl(apiUrl);
-    }
-
 }
