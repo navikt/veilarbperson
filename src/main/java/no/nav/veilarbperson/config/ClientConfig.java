bff777b114a268cfe1d99218c23137e399253808
stianStensli <stian.horn.stensli@nav.no>
2022-09-26 13:29:41 +0200

    service URL mot pdl

--- src/main/java/no/nav/veilarbperson/config/ClientConfig.java
+++ src/main/java/no/nav/veilarbperson/config/ClientConfig.java
@@ -139,7 +139,7 @@ public class ClientConfig {
         String tokenScop = String.format("api://%s.pdl.pdl-api/.default", cluster);
 
         return new PdlClientImpl(
-                internalDevOrProdIngress("pdl-api"),
+                createServiceUrl("pdl-api", "pdl", true),
                 () -> authService.getAadOboTokenForTjeneste(new DownstreamApi(cluster, "pdl", "pdl-api")),
                 () -> tokenClient.createMachineToMachineToken(tokenScop)
         );
