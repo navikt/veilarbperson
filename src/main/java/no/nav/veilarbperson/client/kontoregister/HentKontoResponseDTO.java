b5fbd7fb0107dcffa8c96d52ff2042ab9c8f2e8b
Klara M Helgemo <klara.margrethe.helgemo@nav.no>
2023-05-23 09:47:20 +0200

    TC-369 håndtere status 204 kontonummer finnes ikke i kontoregister

--- src/main/java/no/nav/veilarbperson/client/kontoregister/HentKontoResponseDTO.java
+++ src/main/java/no/nav/veilarbperson/client/kontoregister/HentKontoResponseDTO.java
@@ -1,9 +1,16 @@
 package no.nav.veilarbperson.client.kontoregister;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.http.HttpStatus;
 
 import java.time.LocalDateTime;
-
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
 public class HentKontoResponseDTO {
 
     private HttpStatus status;
