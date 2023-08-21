36ff21db49d559a4a19e12735104eb6059f5f66d
haakonph <hakon.monn-holm@nav.no>
2023-08-21 16:08:59 +0200

    bare returnere true for har nivaaa 4 kallet

--- src/main/java/no/nav/veilarbperson/config/CacheConfig.java
+++ src/main/java/no/nav/veilarbperson/config/CacheConfig.java
@@ -17,7 +17,6 @@ public class CacheConfig {
     public static final String NOM_SKJERMEDE_PERSONER_CACHE_NAME = "nom_skjermede_personer_cache";
     public static final String SIKKERHETSTILTAK_CACHE_NAME = "sikkerhetstiltak_cache";
     public static final String DIGDIR_KONTAKTINFO_CACHE_NAME = "digdir_kontaktinfo_cache";
-    public static final String DIFI_HAR_NIVA_4_CACHE_NAME = "difi_har_niva_4_cache";
     public static final String KODEVERK_BETYDNING_CACHE_NAME = "kodeverk_betydning_cache";
 
     public static final String KONTOREGISTER_CACHE_NAME = "kontoregister_person_cache";
@@ -46,10 +45,6 @@ public class CacheConfig {
         return litenCache(DIGDIR_KONTAKTINFO_CACHE_NAME);
     }
 
-    @Bean
-    public Cache difiHarNiva4Cache() {
-        return litenCache(DIFI_HAR_NIVA_4_CACHE_NAME);
-    }
 
     @Bean
     public Cache kodeverkBetydningCache() {
--- src/main/java/no/nav/veilarbperson/service/UnleashService.java
+++ src/main/java/no/nav/veilarbperson/service/UnleashService.java
@@ -7,16 +7,11 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class UnleashService {
-	private static final String UNLEASH_NIVAA4_DISABLED = "veilarbperson.nivaa4.disabled";
 	private static final String UNLEASH_POAO_TILGANG_ENABLED = "veilarbperson.poao-tilgang-enabled";
 	private final UnleashClient unleashClient;
 
 	public boolean skalBrukePoaoTilgang() {
 		return unleashClient.isEnabled(UNLEASH_POAO_TILGANG_ENABLED);
-	}
-
-	public boolean sjekkNivaa4() {
-		return unleashClient.isEnabled(UNLEASH_NIVAA4_DISABLED);
 	}
 
 }
