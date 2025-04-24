92829abf24ace1728505bb1665f8a660b7742554
Klara M Helgemo <klara.margrethe.helgemo@nav.no>
2025-04-24 14:35:57 +0200

    Fjerner logging av usertoken

--- src/main/java/no/nav/veilarbperson/client/pdl/PdlClientImpl.java
+++ src/main/java/no/nav/veilarbperson/client/pdl/PdlClientImpl.java
@@ -84,7 +84,6 @@ public class PdlClientImpl implements PdlClient {
     @Override
     public HentPerson.PersonNavn hentPersonNavn(PdlRequest pdlRequest) {
         var request = new GqlRequest<>(hentPersonNavnQuery, new GqlVariables.HentPerson(pdlRequest.fnr(), false));
-        log.info("userToken i HentPerson.PersonNavn {}", userTokenProvider.get());
         return graphqlRequest(request, userTokenProvider.get(), pdlRequest.behandlingsnummer(), HentPerson.HentFullmaktNavn.class).hentPerson;
     }
 
